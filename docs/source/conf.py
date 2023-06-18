d2fe9ed36d11732e95d3089f17c10d52f3468cc4
TGusciora <tgusciora@gmail.com>
2023-06-18 12:28:54 +0200

    2023/06/18
    GP23
    - documentation trying to fix error of not displaying gp23package
    - updating os.path.abpsath in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(__file__, "../../..")))
-
+#sys.path.insert(0, os.path.abspath(os.path.join(__file__, "../../..")))
+sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'GP23_PredictingHouseSalePrices'
