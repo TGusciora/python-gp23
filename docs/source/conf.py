cca09b2c4b68ca2adc8f605cf1e0c43e182b0265
TGusciora <tgusciora@gmail.com>
2023-06-21 08:07:52 +0200

    2023/06/21 v2
    GP23
    - further debugging

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,8 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['numpydoc',
-              'sphinx_rtd_theme'
+              'sphinx_rtd_theme',
+              'sphinx.ext.githubpages'
               ]
 autodoc_typehints = 'signature'
 
