46228c3ecb25bca8f6fd4b6eb859457afa4877c2
TGusciora <tgusciora@gmail.com>
2024-02-09 18:52:14 +0100

    2024/02/09 update
    - change package name to src

--- gp23package/__init__.py
+++
@@ -1,8 +0,0 @@
-# Import submodules
-
-# data.make_dataset - importing dataset
-import gp23package.data
-import gp23package.explore_visualise
-import gp23package.features
-import gp23package.models
-import gp23package.utils
---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# Import submodules
+
+# data.make_dataset - importing dataset
+import src.data
+import src.explore_visualise
+import src.features
+import src.models
+import src.utils
